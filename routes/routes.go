d2cd4044a8b9417ba3ee846fa0d595a4a43a5d99
Tanmay Pandey <tanmaypandey1998@gmail.com>
2021-08-11 20:50:59 +0530

    Add routes for CRUD operations

--- routes/routes.go
+++ routes/routes.go
@@ -1,15 +1,14 @@
 package routes
 
 import (
-    "github.com/gofiber/fiber/v2"
-    controllers "./controllers/recordsController"
-//  /give @s minecraft:rick roo   // "github.com/mikefmeyer/catchphrase-go-mongodb-rest-api/controllers" // replace
+	"github.com/gofiber/fiber/v2"
+	"api_golang/controllers"
 )
 
 func RecordsRoute(route fiber.Router) {
-    // route.Get("/", controllers.GetAllCatchphrases)
-    route.Get("/:id", controllers.CreateRecord)
-    // route.Post("/", controllers.AddCatchphrase)
-    // route.Put("/:id", controllers.UpdateCatchphrase)
-    route.Delete("/:id", controllers.DeleteOne)
+	route.Post("/addRecord", controllers.AddRecord)
+    route.Delete("/:id", controllers.DeleteRecord)
+    route.Get("/:id", controllers.GetRecord)
+    route.Put("/:id", controllers.UpdateRecord)
+
 }
