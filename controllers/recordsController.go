c712bce340c096048c2c273a31824acfd8b1eba6
Tanmay Pandey <tanmaypandey1998@gmail.com>
2021-08-11 20:50:30 +0530

    Add CRUD operations

--- controllers/recordsController.go
+++ controllers/recordsController.go
@@ -1,71 +1,228 @@
 package controllers
 
 import (
-	"context"
-	"connectionhelper"
+	connectionhelper "api_golang/db"
+	models "api_golang/records"
+	"log"
+	"os"
+	"time"
+
+	"github.com/gofiber/fiber/v2"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
 )
 
-//CreateRecord - Insert a new document in the collection.
-func CreateRecord(task Records) error {
-	//Get MongoDB connection using connectionhelper.
-	client, err := connectionhelper.GetMongoClient()
+func GetRecord(c *fiber.Ctx) error {
+	recordCollection := connectionhelper.MI.DB.Collection(os.Getenv("COLL"))
+
+	// get param
+	paramID := c.Params("id")
+
+	// convert parameter to object id
+	id, err := primitive.ObjectIDFromHex(paramID)
+
+	// if parameter cannot parse
 	if err != nil {
-		return err
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"success": false,
+			"message": "Cannot parse id",
+			"error":   err,
+		})
 	}
-	//Create a handle to the respective collection in the database.
-	collection := client.Database(connectionhelper.DB).Collection(connectionhelper.ISSUES)
-	//Perform InsertOne operation & validate against the error.
-	_, err = collection.InsertOne(context.TODO(), task)
-	if err != nil {
-		return err
+
+	// find and delete todo
+	// get the inserted data
+	record := &models.Records{}
+	query := bson.D{{Key: "_id", Value: id}}
+
+	recordCollection.FindOne(c.Context(), query).Decode(record)
+
+	err = recordCollection.FindOne(c.Context(), query).Err()
+
+	if err == mongo.ErrNoDocuments {
+		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
+			"success": false,
+			"message": "Record Not found",
+			"error":   err,
+		})
 	}
-	//Return success without any error.
-	return nil
+
+	return c.Status(fiber.StatusCreated).JSON(fiber.Map{
+		"success": true,
+		"data": fiber.Map{
+			"record": record,
+		},
+	})
+
 }
 
-// MarkCompleted - MarkCompleted
-func MarkCompleted(code string) error {
-	//Define filter query for fetching specific document from collection
-	filter := bson.D{primitive.E{Key: "code", Value: code}}
+func AddRecord(c *fiber.Ctx) error {
+	recordCollection := connectionhelper.MI.DB.Collection(os.Getenv("COLL"))
 
-	//Define updater for to specifiy change to be updated.
-	updater := bson.D{primitive.E{Key: "$set", Value: bson.D{
-		primitive.E{Key: "completed", Value: true},
-	}}}
+	data := new(models.Records)
 
-	//Get MongoDB connection using connectionhelper.
-	client, err := connectionhelper.GetMongoClient()
+	err := c.BodyParser(&data)
+
+	// if error
 	if err != nil {
-		return err
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"success": false,
+			"message": "Cannot parse JSON",
+			"error":   err,
+		})
 	}
-	collection := client.Database(connectionhelper.DB).Collection(connectionhelper.ISSUES)
 
-	//Perform UpdateOne operation & validate against the error.
-	_, err = collection.UpdateOne(context.TODO(), filter, updater)
+	// data.ID = nil
+	// data.Name = "hmm"
+	// data.Completed = &f
+	data.CreatedAt = time.Now()
+	// data.UpdatedAt = time.Now()
+
 	if err != nil {
-		return err
+		log.Println(err)
+		return c.Status(400).JSON(fiber.Map{
+			"success": false,
+			"message": "Failed to parse body",
+			"error":   err,
+		})
 	}
-	//Return success without any error.
-	return nil
+
+	result, err := recordCollection.InsertOne(c.Context(), data)
+	if err != nil {
+		return c.Status(500).JSON(fiber.Map{
+			"success": false,
+			"message": "Record failed to insert",
+			"error":   err,
+		})
+	}
+
+	return c.Status(fiber.StatusCreated).JSON(fiber.Map{
+		"data":    result,
+		"success": true,
+		"message": "Record inserted successfully",
+	})
+
 }
 
-func DeleteOne(code string) error {
-	//Define filter query for fetching specific document from collection
-	filter := bson.D{primitive.E{Key: "code", Value: code}}
-	//Get MongoDB connection using connectionhelper.
-	client, err := connectionhelper.GetMongoClient()
+// DeleteTodo : Delete a todo
+// PARAM: id
+func DeleteRecord(c *fiber.Ctx) error {
+	recordCollection := connectionhelper.MI.DB.Collection(os.Getenv("COLL"))
+
+	// get param
+	paramID := c.Params("id")
+
+	// convert parameter to object id
+	id, err := primitive.ObjectIDFromHex(paramID)
+
+	// if parameter cannot parse
 	if err != nil {
-		return err
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"success": false,
+			"message": "Cannot parse id",
+			"error":   err,
+		})
 	}
-	//Create a handle to the respective collection in the database.
-	collection := client.Database(connectionhelper.DB).Collection(connectionhelper.ISSUES)
-	//Perform DeleteOne operation & validate against the error.
-	_, err = collection.DeleteOne(context.TODO(), filter)
+
+	// find and delete todo
+	query := bson.D{{Key: "_id", Value: id}}
+
+	err = recordCollection.FindOneAndDelete(c.Context(), query).Err()
+
 	if err != nil {
-		return err
+		if err == mongo.ErrNoDocuments {
+			return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
+				"success": false,
+				"message": "Record Not found",
+				"error":   err,
+			})
+		}
+
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"success": false,
+			"message": "Cannot delete record",
+			"error":   err,
+		})
 	}
-	//Return success without any error.
-	return nil
+
+	return c.SendStatus(fiber.StatusNoContent)
+}
+
+// UpdateTodo : Update a todo
+// PARAM: id
+func UpdateRecord(c *fiber.Ctx) error {
+	recordCollection := connectionhelper.MI.DB.Collection(os.Getenv("COLL"))
+
+	// find parameter
+	paramID := c.Params("id")
+
+	// convert parameterID to objectId
+	id, err := primitive.ObjectIDFromHex(paramID)
+
+	// if parameter cannot parse
+	if err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"success": false,
+			"message": "Cannot parse id",
+			"error":   err,
+		})
+	}
+
+	// var data Request
+	data := new(models.Records)
+	err = c.BodyParser(&data)
+
+	if err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"success": false,
+			"message": "Cannot parse JSON",
+			"error":   err,
+		})
+	}
+
+	query := bson.D{{Key: "_id", Value: id}}
+
+	// updateData
+	var dataToUpdate bson.D
+
+	if data.Name != "" {
+		// todo.Title = *data.Title
+		dataToUpdate = append(dataToUpdate, bson.E{Key: "name", Value: data.Name})
+	}
+
+	update := bson.D{
+		{Key: "$set", Value: dataToUpdate},
+	}
+
+	// update
+	err = recordCollection.FindOneAndUpdate(c.Context(), query, update).Err()
+
+	if err != nil {
+		if err == mongo.ErrNoDocuments {
+			return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
+				"success": false,
+				"message": "Record Not found",
+				"error":   err,
+			})
+		}
+
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"success": false,
+			"message": "Cannot update todo",
+			"error":   err,
+		})
+	}
+
+	// get updated data
+	record := &models.Records{}
+
+	recordCollection.FindOne(c.Context(), query).Decode(record)
+
+	return c.Status(fiber.StatusOK).JSON(fiber.Map{
+		"success": true,
+		"data": fiber.Map{
+			"record": record,
+		},
+	})
 }
